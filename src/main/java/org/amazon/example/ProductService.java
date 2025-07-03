e8901d96386c1bc0e74b8e93e53fd2a0220c1812
Ana Diru <ajd6735@psu.edu>
2025-07-03 18:55:25 +0000

    adds a product repository interface, a product entity and  a product service

---
+++ src/main/java/org/amazon/example/ProductService.java
@@ -0,0 +1,29 @@
+package org.amazon.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+    @Autowired
+    ProductRepository productRepository;
+
+    public Product save(Product product){
+        return (Product) productRepository.save(product);
+    }
+
+    public List<Product> findAll(){
+        return productRepository.findAll();
+    }
+
+    public Optional<Product> findById(int id){
+        return productRepository.findById(id);
+    }
+
+    public void deleteById(int id){
+        productRepository.deleteById(id);
+    }
+}
